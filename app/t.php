ac8af6405531dac9dc57fe31274086499f4a1092
jackyzhou <jackyzhou@lean-soft.cn>
2017-02-10 11:36:08 +0800

    init project

---
+++ app/t.php
@@ -0,0 +1,9 @@
+<?php
+
+$t = 7000;
+echo $t;
+$tt= 100000;
+$ttt=$t+$tt;
+echo $ttt;
+echo "asdfasdfdsafadsfads";
+?>
---
+++ app/zhou.php
@@ -0,0 +1,7 @@
+<?php
+$ooo='ooo';
+$age = 10;
+$age2=12;
+$total=$age+$age2;
+phpinfo();
+?>
